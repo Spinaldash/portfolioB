f946a2b1f1b9a68f292f795ebaf6d6bf464af0c0
Spinaldash <blueavenger@gmail.com>
2015-05-07 17:39:35 -0700

    atempting to fix for deply

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('web-portfolio', ['firebase', 'ui.router', 'ngAnimate']);
+angular.module('web-portfolio', ['firebase', 'ui.router']);
 
 (function($){
   $(function(){
